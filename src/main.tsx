945a7000bd472439a31ae41ed0cd4aa867bd68f3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-19 20:08:37 +0200

    changed import path of index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App.tsx";
-import "./core/index.css";
+import "./index.css";
 import { Provider } from "react-redux";
 import store from "./core/store.ts";
 
