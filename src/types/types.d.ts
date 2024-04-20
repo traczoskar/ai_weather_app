827c7210e72042d7c98cab231108aa82c964c6de
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-20 21:43:39 +0200

    created WeatherResponse type

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -9,3 +9,38 @@ export interface GeocodingData extends GeocodingResponse {
   country: string;
   state?: string;
 }
+
+export interface WeatherResponse {
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+  };
+  weather: [
+    {
+      description: string;
+      main: string;
+    }
+  ];
+  coord: GeocodingResponse;
+  clouds: {
+    all: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  dt: number;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+}
