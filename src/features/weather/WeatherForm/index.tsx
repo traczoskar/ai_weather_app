67b1e2002473becda0d2179f26536aadebbd5699
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-21 01:03:57 +0200

    changed styling of form input and added location icon handling

--- src/features/weather/WeatherForm/index.tsx
+++ src/features/weather/WeatherForm/index.tsx
@@ -49,6 +49,17 @@ function WeatherForm() {
   //   }
   // };
 
+  const handleLocation = (e) => {
+    e.preventDefault();
+    console.log("Button clicked");
+    if (navigator.geolocation) {
+      console.log("Geolocation is available");
+      navigator.geolocation.getCurrentPosition(showPosition, handleError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  };
+
   const handleError = (error: GeolocationPositionError) => {
     console.error("Error obtaining location: ", error.message);
     alert(`Error obtaining location: ${error.message}`);
@@ -72,77 +83,77 @@ function WeatherForm() {
   return (
     <TranspContainer>
       <form className="flex justify-center align-center">
-        <div className="flex flex-col">
-          <label className="relative block">
-            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+        <label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
+                className="w-4 h-4  text-gray-400"
+                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+                viewBox="0 0 20 20"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
-            </span>
+            </div>
+            <div
+              onClick={handleLocation}
+              className="absolute inset-y-0 end-2 flex items-center ps-3 hover:cursor-pointer"
+            >
+              <svg
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
+                id="fi_2136283"
+                enable-background="new 0 0 512 512"
+                height="512"
+                viewBox="0 0 512 512"
+                width="512"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="currentColor"
+                  d="m497 241h-19.939c-3.556-53.537-26.094-103.381-64.386-141.675-38.294-38.293-88.138-60.831-141.675-64.386v-19.939c0-8.284-6.716-15-15-15s-15 6.716-15 15v19.939c-53.537 3.556-103.381 26.093-141.675 64.387-38.293 38.293-60.831 88.137-64.386 141.674h-19.939c-8.284 0-15 6.716-15 15s6.716 15 15 15h19.939c3.556 53.537 26.094 103.381 64.386 141.674 38.294 38.294 88.138 60.831 141.675 64.387v19.939c0 8.284 6.716 15 15 15s15-6.716 15-15v-19.939c53.537-3.556 103.381-26.093 141.675-64.387 38.293-38.293 60.831-88.138 64.386-141.674h19.939c8.284 0 15-6.716 15-15s-6.716-15-15-15zm-226 205.986v-18.844c0-8.284-6.716-15-15-15s-15 6.716-15 15v18.844c-93.698-7.291-168.695-82.288-175.986-175.986h18.844c8.284 0 15-6.716 15-15s-6.716-15-15-15h-18.844c7.291-93.698 82.288-168.695 175.986-175.986v18.843c0 8.284 6.716 15 15 15s15-6.716 15-15v-18.843c93.698 7.291 168.695 82.288 175.986 175.986h-18.844c-8.284 0-15 6.716-15 15s6.716 15 15 15h18.844c-7.291 93.698-82.288 168.695-175.986 175.986z"
+                ></path>
+                <path
+                  fill="currentColor"
+                  d="m256 181.324c-41.177 0-74.676 33.5-74.676 74.676s33.499 74.676 74.676 74.676 74.676-33.5 74.676-74.676-33.499-74.676-74.676-74.676zm0 119.352c-24.635 0-44.676-20.042-44.676-44.676s20.041-44.676 44.676-44.676 44.676 20.042 44.676 44.676-20.041 44.676-44.676 44.676z"
+                ></path>
+              </svg>
+            </div>
             <input
               type="text"
               value={cityName}
               onChange={handleLocationInput}
               placeholder="Enter city name..."
-              className="input placeholder:text-neutral-400 hover:bg-neutral-600 hover:border-neutral-200
-        hover:cursor-pointer focus:outline-none w-200"
+              className="block w-full p-4 ps-10 text-sm  border border-neutral-600 rounded-lg bg-gray-700  focus:ring-blue-500 focus:border-gray-300 placeholder:text-neutral-400 hover:bg-gray-600 
+        focus:outline-none w-200"
             ></input>
-          </label>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("Button clicked");
-              if (navigator.geolocation) {
-                console.log("Geolocation is available");
-                navigator.geolocation.getCurrentPosition(
-                  showPosition,
-                  handleError
-                );
-              } else {
-                alert("Geolocation is not supported by this browser.");
-              }
-            }}
-          >
-            Lokalizacja
-          </button>
-          {geocodingResponse && geocodingResponse.length > 0 && (
-            <div className="">
-              {geocodingResponse.map(
-                (location: GeocodingData, index: number) => (
-                  <Select
-                    key={index}
-                    onClick={() =>
-                      dispatch(
-                        fetchWeather({ lat: location.lat, lon: location.lon })
-                      )
-                    }
-                  >
-                    {location.name}, {location.country}, ({location.state})
-                  </Select>
-                )
-              )}
-            </div>
-          )}
-        </div>
+          </div>
+        </label>
 
-        <button
-          disabled
-          type="submit"
-          className="btn  hover:bg-neutral-600 hover:border-neutral-200 disabled:opacity-20"
-        >
-          Get Weather
-        </button>
+        {geocodingResponse && geocodingResponse.length > 0 && (
+          <div className="">
+            {geocodingResponse.map((location: GeocodingData, index: number) => (
+              <Select
+                key={index}
+                onClick={() =>
+                  dispatch(
+                    fetchWeather({ lat: location.lat, lon: location.lon })
+                  )
+                }
+              >
+                {location.name}, {location.country}, ({location.state})
+              </Select>
+            ))}
+          </div>
+        )}
+        {status === "loading" && <div>Loading...</div>}
       </form>
     </TranspContainer>
   );
