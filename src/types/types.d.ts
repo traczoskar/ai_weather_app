bb7b2b0684d9a4c0f92167ca00157b0bfc34de8d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-20 09:52:08 +0200

    created new type and changed naming

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,11 @@
-export interface GeocodingData {
+export interface GeocodingResponse {
   lat: number;
   lon: number;
+}
+
+export interface GeocodingData extends GeocodingResponse {
+  name: string;
+  local_names?: Record<string, any>;
+  country: string;
+  state?: string;
 }
