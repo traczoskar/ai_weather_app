e8e78447cdb3a6f11eeeaf4589f5a65875d8e17f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-20 21:42:57 +0200

    created button which gets user location from Navigator instance to fetch weather data

--- src/features/weather/WeatherForm/index.tsx
+++ src/features/weather/WeatherForm/index.tsx
@@ -10,6 +10,7 @@ import {
 } from "../../../slices/apiDataSlice";
 import { useState } from "react";
 import Select from "../../../components/Select";
+import TranspContainer from "../../../components/TranspContainer";
 
 function WeatherForm() {
   const [cityName, setCityName] = useState<string>("");
@@ -31,79 +32,119 @@ function WeatherForm() {
     }
   };
 
-  const getGeocodingData = async () => {
-    if (geocodingResponse) {
-      const geocodingData: GeocodingResponse = {
-        lat: geocodingResponse[0].lat,
-        lon: geocodingResponse[0].lon,
-      };
-      console.log(geocodingData);
-      return geocodingData;
-    }
+  // const getGeocodingData = async () => {
+  //   if (geocodingResponse) {
+  //     const geocodingData: GeocodingResponse = {
+  //       lat: geocodingResponse[0].lat,
+  //       lon: geocodingResponse[0].lon,
+  //     };
+  //     console.log(geocodingData);
+  //     return geocodingData;
+  //   }
+  // };
+
+  // const handleFetchWeather = async () => {
+  //   const geocodingData = await getGeocodingData();
+  //   if (geocodingData) {
+  //   }
+  // };
+
+  const handleError = (error: GeolocationPositionError) => {
+    console.error("Error obtaining location: ", error.message);
+    alert(`Error obtaining location: ${error.message}`);
   };
 
-  const handleFetchWeather = async () => {
-    const geocodingData = await getGeocodingData();
-    if (geocodingData) {
-      dispatch(fetchWeather(geocodingData as any));
-    }
+  const showPosition = (position: GeolocationPosition) => {
+    console.log(
+      "Fetching weather for:",
+      position.coords.latitude,
+      position.coords.longitude
+    );
+    dispatch(
+      fetchWeather({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      })
+    );
+    console.log(weatherResponse);
   };
 
   return (
-    <form className="flex justify-center align-center">
-      <div className="flex flex-col">
-        <label className="relative block">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              />
-            </svg>
-          </span>
-          <input
-            type="text"
-            value={cityName}
-            onChange={handleLocationInput}
-            placeholder="Enter city name..."
-            className="input placeholder:text-neutral-400 hover:bg-neutral-600 hover:border-neutral-200
-          hover:cursor-pointer focus:outline-none"
-          ></input>
-        </label>
-        {geocodingResponse && geocodingResponse.length > 0 && (
-          <div className="">
-            {geocodingResponse.map((location: GeocodingData, index: number) => (
-              <Select
-                key={index}
-                onClick={() =>
-                  dispatch(
-                    fetchWeather({ lat: location.lat, lon: location.lon })
-                  )
-                }
+    <TranspContainer>
+      <form className="flex justify-center align-center">
+        <div className="flex flex-col">
+          <label className="relative block">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
               >
-                {location.name}, {location.country}, ({location.state})
-              </Select>
-            ))}
-          </div>
-        )}
-      </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </span>
+            <input
+              type="text"
+              value={cityName}
+              onChange={handleLocationInput}
+              placeholder="Enter city name..."
+              className="input placeholder:text-neutral-400 hover:bg-neutral-600 hover:border-neutral-200
+        hover:cursor-pointer focus:outline-none w-200"
+            ></input>
+          </label>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("Button clicked");
+              if (navigator.geolocation) {
+                console.log("Geolocation is available");
+                navigator.geolocation.getCurrentPosition(
+                  showPosition,
+                  handleError
+                );
+              } else {
+                alert("Geolocation is not supported by this browser.");
+              }
+            }}
+          >
+            Lokalizacja
+          </button>
+          {geocodingResponse && geocodingResponse.length > 0 && (
+            <div className="">
+              {geocodingResponse.map(
+                (location: GeocodingData, index: number) => (
+                  <Select
+                    key={index}
+                    onClick={() =>
+                      dispatch(
+                        fetchWeather({ lat: location.lat, lon: location.lon })
+                      )
+                    }
+                  >
+                    {location.name}, {location.country}, ({location.state})
+                  </Select>
+                )
+              )}
+            </div>
+          )}
+        </div>
 
-      <button
-        disabled
-        type="submit"
-        className="btn  hover:bg-neutral-600 hover:border-neutral-200 disabled:opacity-20"
-      >
-        Get Weather
-      </button>
-    </form>
+        <button
+          disabled
+          type="submit"
+          className="btn  hover:bg-neutral-600 hover:border-neutral-200 disabled:opacity-20"
+        >
+          Get Weather
+        </button>
+      </form>
+    </TranspContainer>
   );
 }
 
