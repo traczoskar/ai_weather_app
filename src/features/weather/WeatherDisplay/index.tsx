0d4224d9ea86bdad4da713485a913c93ba4b0fff
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-20 21:42:06 +0200

    added first animated icon to try possibilities

--- src/features/weather/WeatherDisplay/index.tsx
+++ src/features/weather/WeatherDisplay/index.tsx
@@ -5,16 +5,28 @@ import {
   selectStatus,
   selectWeatherData,
 } from "../../../slices/apiDataSlice";
+import Lottie from "react-lottie";
+import rainy_cloud from "../../../assets/animations/rainy_cloud.json";
 
 function WeatherDisplay() {
   const status = useSelector(selectStatus);
   const error = useSelector(selectError);
   const weatherResponse = useSelector(selectWeatherData);
-  const geocodingResponse = useSelector(selectGeoCodingData);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: rainy_cloud, // wcześniej zaimportowane dane animacji
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
-    <section className="flex justify-center align-center">
+    <section className="flex justify-center mt-10">
       <h2>Weather Display</h2>
+      <div>
+        <Lottie options={defaultOptions} height={50} width={50} />
+      </div>
     </section>
   );
 }
