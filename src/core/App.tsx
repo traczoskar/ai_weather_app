7ac428e5e549272dc8e187018a444881ce6958bf
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-21 01:03:34 +0200

    changed styling for <main> tag

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 export default function App() {
   return (
-    <main className="flex flex-col bg-custom-bg bg-cover bg-center h-full m-16 rounded-3xl border-solid border-2">
+    <main className="flex flex-col items-center bg-custom-bg bg-cover bg-center h-full m-16 rounded-3xl border-solid border-2">
       <Header title="WeatherWise.ai" />
       <header className="flex justify-center align-center">
         <h1 className="text-4xl font-bold py-20">Weather App</h1>
