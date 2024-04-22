edefb2c2a0abc2153d5207d847a77d462175adee
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-22 17:23:51 +0200

    moved WeatherForm to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
+import WeatherForm from "../../features/weather/WeatherForm";
+
 interface HeaderProps {
   title: string;
 }
 
 const Header = ({ title }: HeaderProps) => {
   return (
-    <header className="flex justify-center items-center bg-black/10 backdrop-blur-md p-6 max-w-4xl rounded-3xl">
+    <header className="flex justify-center items-center gap-12 bg-black/10 backdrop-blur-md p-6 w-full rounded-3xl">
       <h1 className="text-4xl font-bold font-['Raleway'] ">{title}</h1>
+      <WeatherForm />
     </header>
   );
 };
