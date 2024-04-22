db81fbcc39afa99c9c8ccd1845fb87b5229d7ced
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-22 17:25:03 +0200

    removed header section from App.tsx

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -6,10 +6,6 @@ export default function App() {
   return (
     <main className="flex flex-col items-center bg-custom-bg bg-cover bg-center h-full m-16 rounded-3xl border-solid border-2">
       <Header title="WeatherWise.ai" />
-      <header className="flex justify-center align-center">
-        <h1 className="text-4xl font-bold py-20">Weather App</h1>
-      </header>
-      <WeatherForm />
       <WeatherDisplay />
     </main>
   );
