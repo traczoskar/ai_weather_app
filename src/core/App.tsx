cbac0f97d41a3bc5158b6e4984477331414d9fb8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-23 22:02:24 +0200

    added initial SuggestionDisplay component to App.tsx

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,12 +1,15 @@
-import WeatherForm from "../features/weather/WeatherForm";
 import WeatherDisplay from "../features/weather/WeatherDisplay";
 import Header from "../components/Header";
+import SuggestionDisplay from "../features/suggestions/SuggestionDisplay";
 
 export default function App() {
   return (
     <main className="flex flex-col items-center bg-custom-bg bg-cover bg-center h-full m-16 rounded-3xl border-solid border-2">
       <Header title="WeatherWise.ai" />
-      <WeatherDisplay />
+      <div className="flex items-center gap-8">
+        <WeatherDisplay />
+        <SuggestionDisplay message="It's a good day to go out!" />
+      </div>
     </main>
   );
 }
