f5e3263a30594fb03e106966576def2855af6016
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-24 14:03:19 +0200

    added query initial state to aiCompletionSlice

--- src/slices/aiCompletionSlice.ts
+++ src/slices/aiCompletionSlice.ts
@@ -1,9 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AICompletionState } from "../types/types";
+import { AICompletionState, AIQuery } from "../types/types";
 
 const initialState: AICompletionState = {
   isLoading: false,
   error: null,
+  query: {
+    systemMessage: "",
+    userMessage: "",
+  },
   response: null,
 };
 
@@ -11,9 +15,10 @@ const aiCompletionSlice = createSlice({
   name: "aiCompletion",
   initialState,
   reducers: {
-    startLoading: (state) => {
+    setQuery: (state, action: PayloadAction<AIQuery>) => {
       state.isLoading = true;
       state.error = null;
+      state.query = action.payload;
       state.response = null;
     },
     setLoadingError: (state, action: PayloadAction<string>) => {
@@ -29,12 +34,14 @@ const aiCompletionSlice = createSlice({
   },
 });
 
-export const { startLoading, setLoadingError, setResponse } =
+export const { setQuery, setLoadingError, setResponse } =
   aiCompletionSlice.actions;
 
 export const selectAICompletionState = (state: any) => state.aiCompletion;
 export const selectAIIsLoading = (state: any) =>
   selectAICompletionState(state).isLoading;
+export const selectAIQuery = (state: any) =>
+  selectAICompletionState(state).query;
 export const selectAIError = (state: any) =>
   selectAICompletionState(state).error;
 export const selectAIResponse = (state: any) =>
