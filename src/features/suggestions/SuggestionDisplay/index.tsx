d7b31251a572c44b642fdebfbfbc4afbb19ee18f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-24 17:13:31 +0200

    re-factored SuggestionDisplay component for better aiResponse rendering

--- src/features/suggestions/SuggestionDisplay/index.tsx
+++ src/features/suggestions/SuggestionDisplay/index.tsx
@@ -1,18 +1,26 @@
+import { useSelector } from "react-redux";
 import TranspContainer from "../../../components/TranspContainer";
+import { selectAIResponse } from "../../../slices/aiCompletionSlice";
+import ReactMarkdown from "react-markdown";
 
-interface SuggestionDisplayProps {
-  message: string;
-}
-
-const SuggestionDisplay = ({ message }: SuggestionDisplayProps) => {
+const SuggestionDisplay = () => {
+  const aiResponse = useSelector(selectAIResponse);
   return (
-    <TranspContainer>
-      <section className="flex flex-col w-96">
-        <h2 className="text-2xl font-bold mb-4">AI suggestion:</h2>
-        <p>{message}</p>
-      </section>
-    </TranspContainer>
+    <>
+      {aiResponse && (
+        <TranspContainer>
+          <section className="flex flex-col w-96">
+            <h2 className="text-2xl font-bold mb-4">AI suggestion:</h2>
+            <article className="text-sm text-gray-200">
+              {/* <p className="text-sm font-normal py-4 whitespace-pre-line">
+          {aiResponse}
+        </p> */}
+              <ReactMarkdown>{aiResponse}</ReactMarkdown>
+            </article>
+          </section>
+        </TranspContainer>
+      )}
+    </>
   );
 };
-
 export default SuggestionDisplay;
