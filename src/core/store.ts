82d4e3cb71257715322ba0915eee73d9a20e1415
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-04-24 14:04:07 +0200

    added aiCompletionReducer to Redux store

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiDataReducer from "../slices/apiDataSlice";
+import aiCompletionReducer from "../slices/aiCompletionSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "../sagas/saga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     apiData: apiDataReducer,
+    aiCompletion: aiCompletionReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
