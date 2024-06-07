6e4826f9e0d369a7dc0d4c825c03b3cebc3c4c75
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-07 13:25:54 +0300

    refactor: Update main app layout and styling

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -4,8 +4,9 @@ import SuggestionDisplay from "../features/suggestions/SuggestionDisplay";
 
 export default function App() {
   return (
-    <main className="flex flex-col items-center bg-custom-bg bg-cover bg-center h-full m-16 rounded-3xl border-solid border-2">
+    <main className="flex flex-col items-center h-full max-w-screen-lg mx-auto">
       <Header title="WeatherWise.ai" />
+
       <div className="flex flex-col items-center gap-8 mx-8 my-2">
         <WeatherDisplay />
         <SuggestionDisplay />
