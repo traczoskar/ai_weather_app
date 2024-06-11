cd2451cc56388433bd4dff9e2219c42294d4d50b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-11 15:57:01 +0200

    refactor: Update Select component styles to use gray background color

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -7,8 +7,8 @@ const Select = ({ onClick, children }: SelectProps) => {
   return (
     <div
       onClick={onClick}
-      className="px-3 py-2 text-sm text-neutral-400 bg-neutral-800
-        hover:bg-neutral-600 hover:border-neutral-200 hover:cursor-pointer"
+      className="px-3 py-2 text-sm text-gray-800 bg-neutral-100
+        hover:bg-neutral-200 hover:border-neutral-300 hover:cursor-pointer"
     >
       {children}
     </div>
