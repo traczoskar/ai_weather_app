a610b3d23f714a2ef39f5336daa9f0bcffe5fe7d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-11 19:44:42 +0200

    refactor: Update main.tsx to include QueryClientProvider and ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import App from "./core/App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./core/store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ReactQueryDevtools />
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
