8825493b6d13b22f2fa79ce8f476b7886f7f31c4
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-11 19:45:38 +0200

    Update App component to handle fetching weather data and location update functionality

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,15 +2,39 @@ import WeatherDisplay from "../features/weather/WeatherDisplay";
 import Header from "../components/Header";
 import SuggestionDisplay from "../features/suggestions/SuggestionDisplay";
 
-export default function App() {
+import { useWeatherData } from "../api/useWeatherData";
+import { useEffect, useState } from "react";
+import { GeocodingData } from "../types/types";
+
+const App = () => {
+  const [selectedLocation, setSelectedLocation] =
+    useState<GeocodingData | null>(null);
+
+  const { isPending, data, error } = useWeatherData(
+    selectedLocation?.lat || null,
+    selectedLocation?.lon || null
+  );
+
+  useEffect(() => {
+    console.log("isPending: ", isPending);
+    console.log("data: ", data);
+    console.log("error: ", error);
+  }, [isPending, data, error]);
+
   return (
     <main className="flex flex-col items-center gap-8">
-      <Header title="WeatherWise.ai" />
+      <Header
+        title="WeatherWise.ai"
+        updateLocation={setSelectedLocation}
+        weatherData={{ isPending, data, error }}
+      />
 
       <div className="flex flex-col items-center gap-8 w-full">
         <SuggestionDisplay />
-        <WeatherDisplay />
+        <WeatherDisplay weatherData={{ isPending, data, error }} />
       </div>
     </main>
   );
-}
+};
+
+export default App;
