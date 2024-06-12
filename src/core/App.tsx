f6c0b6a72bd812f99bccee83670085a0c32465bb
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-12 19:53:41 +0200

    implemented AI completion logic in App.tsx

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -5,12 +5,29 @@ import SuggestionDisplay from "../features/suggestions/SuggestionDisplay";
 import { useWeatherData } from "../api/useWeatherData";
 import { useEffect, useState } from "react";
 import { GeocodingData } from "../types/types";
+import { usePromptDataBase } from "../openAI/usePromptDataBase";
+import { useAIResponse } from "../openAI/useAIResponse";
+
+interface AiData {
+  isPending: boolean;
+  data: any | null;
+  error: any | null;
+}
 
 const App = () => {
   const [selectedLocation, setSelectedLocation] =
     useState<GeocodingData | null>(null);
-
-  const { isPending, data, error } = useWeatherData(
+  const [aiData, setAiData] = useState<AiData>({
+    isPending: false,
+    data: null,
+    error: null,
+  });
+  const {
+    isPending,
+    isFetching: isWeatherFetching,
+    data,
+    error,
+  } = useWeatherData(
     selectedLocation?.lat || null,
     selectedLocation?.lon || null
   );
@@ -21,6 +38,49 @@ const App = () => {
     console.log("error: ", error);
   }, [isPending, data, error]);
 
+  const prompt = usePromptDataBase(data);
+
+  const {
+    refetch,
+    isFetching,
+    data: aiResponse,
+    error: aiError,
+  } = useAIResponse(prompt.systemMessage, prompt.userMessage);
+
+  const getWeatherAdvice = async () => {
+    setAiData((prev) => ({
+      ...prev,
+      isPending: true,
+    }));
+    try {
+      const { data } = await refetch();
+      setAiData({
+        isPending: false,
+        data: data,
+        error: null,
+      });
+    } catch (error) {
+      setAiData({
+        isPending: false,
+        data: null,
+        error,
+      });
+    }
+  };
+
+  useEffect(() => {
+    setAiData((prev) => ({
+      ...prev,
+      isPending: isFetching,
+    }));
+  }, [isFetching]);
+
+  useEffect(() => {
+    console.log("aiData: ", aiData.data);
+    console.log("aiData error: ", aiData.error);
+    console.log("aiData isPending: ", aiData.isPending);
+  }, [aiData.data, aiData.error, aiData.isPending]);
+
   return (
     <main className="flex flex-col items-center gap-8">
       <Header
@@ -30,8 +90,12 @@ const App = () => {
       />
 
       <div className="flex flex-col items-center gap-8 w-full">
-        <SuggestionDisplay />
-        <WeatherDisplay weatherData={{ isPending, data, error }} />
+        <SuggestionDisplay aiData={aiData} />
+        <WeatherDisplay
+          aiData={aiData}
+          weatherData={{ isPending, isWeatherFetching, data, error }}
+          aiRequest={getWeatherAdvice}
+        />
       </div>
     </main>
   );
