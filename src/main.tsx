a62de9c73d05e39d7fa16a7f2ea440dd52e6ecf3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-13 12:31:50 +0200

    deleted Redux store Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./core/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -12,10 +10,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ReactQueryDevtools />
-        <App />
-      </Provider>
+      <ReactQueryDevtools />
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
