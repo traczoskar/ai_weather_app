efe4116a7b1eec58030d88de2fbb1abf84d40278
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-13 19:43:47 +0200

    refactor: Update Detail component to include optional width and textSize props

--- src/features/weather/WeatherDisplay/DetailedInfo/Detail/index.tsx
+++ src/features/weather/WeatherDisplay/DetailedInfo/Detail/index.tsx
@@ -4,13 +4,25 @@ interface DetailProps {
   icon?: ReactNode;
   title: string;
   data: string;
+  width?: string;
+  textSize?: string;
 }
 
-const Detail: React.FC<DetailProps> = ({ icon, title, data }) => {
+const Detail: React.FC<DetailProps> = ({
+  icon,
+  title,
+  data,
+  width,
+  textSize,
+}) => {
   return (
-    <div className="flex items-center gap-2 text-slate-500">
+    <div className="flex items-center gap-4 text-slate-500">
       {icon}
-      <p className="flex items-center gap-2 justify-between w-fulltext-md">
+      <p
+        className={`flex items-center gap-6 justify-between  ${
+          width ? width : "w-full"
+        } ${textSize ? textSize : "text-md"} font-normal}`}
+      >
         {title}{" "}
         <span className="text-sky-600 text-md font-semibold">{data}</span>
       </p>
