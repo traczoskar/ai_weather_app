b6eaab984752491330a85cfd5797f6dfce7c23c8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-13 12:31:31 +0200

    deleted Redux-Saga files

--- src/sagas/saga.ts
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-import { apiDataSaga } from "./apiDataSaga";
-import { aiCompletionSaga } from "./aiCompletionSaga";
-
-export default function* rootSaga() {
-  yield all([apiDataSaga(), aiCompletionSaga()]);
-}
