e8ebae526565e12388040c350ea44c6a5d89fa6a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-13 19:43:40 +0200

    feat: Add "Feels like" detail with feather SVG icon

--- src/features/weather/WeatherDisplay/DetailedInfo/index.tsx
+++ src/features/weather/WeatherDisplay/DetailedInfo/index.tsx
@@ -3,6 +3,7 @@ import {
   formatTemperature,
   formatTime,
 } from "../../../../utils/dataFormatting";
+import FeelsLikeIcon from "../../../../assets/icons/feather.svg?react";
 import ArrowUpIcon from "../../../../assets/icons/arrow-up.svg?react";
 import ArrowDownIcon from "../../../../assets/icons/arrow-down.svg?react";
 import HumidityIcon from "../../../../assets/icons/drop.svg?react";
@@ -23,58 +24,63 @@ const DetailedInfo: React.FC<DetailedInfoProps> = ({ weather }) => {
     <div className="flex flex-col gap-8">
       {weather?.main.feels_like ? (
         <Detail
+          icon={<FeelsLikeIcon width={27} height={27} />}
+          width="w-auto"
+          textSize="text-xl"
           title="Feels like:"
           data={`${formatTemperature(weather.main.feels_like)}°C`}
         />
       ) : null}
-      <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+      <div className="grid grid-cols-2 gap-x-12 gap-y-3">
         <Detail
-          icon={<ArrowUpIcon />}
+          icon={<ArrowUpIcon width={22} height={22} />}
           title="Max:"
           data={`${weather?.main.temp_max}°C`}
         />
         <Detail
-          icon={<ArrowDownIcon />}
+          icon={<ArrowDownIcon width={22} height={22} />}
           title="Min:"
           data={`${weather?.main.temp_min}°C`}
         />
         <Detail
-          icon={<HumidityIcon />}
+          icon={<HumidityIcon width={22} height={22} />}
           title="Humidity:"
           data={`${weather?.main.humidity} %`}
         />
         <Detail
-          icon={<PressureIcon />}
+          icon={<PressureIcon width={22} height={22} />}
           title="Pressure:"
           data={`${weather?.main.pressure} hPa`}
         />
         <Detail
-          icon={<WindIcon />}
+          icon={<WindIcon width={22} height={22} />}
           title="Wind:"
           data={`${weather?.wind.speed} m/s`}
         />
         <Detail
-          icon={<CloudinessIcon />}
+          icon={<CloudinessIcon width={22} height={22} />}
           title="Cloudiness:"
           data={weather?.clouds.all + " %"}
         />
         <Detail
-          icon={<VisibilityIcon />}
+          icon={<VisibilityIcon width={22} height={22} />}
           title="Visibility:"
           data={weather?.visibility + " m"}
         />
       </div>
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-2 gap-x-8 ">
         {weather?.sys.sunrise ? (
           <Detail
-            icon={<SunRiseIcon />}
+            textSize="text-xl"
+            icon={<SunRiseIcon width={27} height={27} />}
             title="Sunrise:"
             data={formatTime(weather.sys.sunrise)}
           />
         ) : null}
         {weather?.sys.sunset ? (
           <Detail
-            icon={<SunSetIcon />}
+            textSize="text-xl"
+            icon={<SunSetIcon width={27} height={27} />}
             title="Sunset:"
             data={formatTime(weather.sys.sunset)}
           />
