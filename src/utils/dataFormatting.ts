52703eb76a9ca1758be24718a17ed61e3460cc90
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-13 19:15:09 +0200

    refactor: Simplify dataFormatting functions and improve code readability, added formatTime function

--- src/utils/dataFormatting.ts
+++ src/utils/dataFormatting.ts
@@ -1,8 +1,8 @@
 // Functions that format data for display
-export const formatDescription = (description: string) => {
-  return description.charAt(0).toUpperCase() + description.slice(1);
-};
+export const formatDescription = (description: string) =>
+  description.charAt(0).toUpperCase() + description.slice(1);
 
-export const formatTemperature = (temp: number) => {
-  return temp.toFixed(1);
-};
+export const formatTemperature = (temp: number) => temp.toFixed(1);
+
+export const formatTime = (time: number) =>
+  new Date(time * 1000).toLocaleTimeString();
