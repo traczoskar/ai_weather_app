8b41ac64a6591084348ff6403f49955f24678072
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-14 17:34:09 +0200

    refactor: Update WeatherDisplay component to improve UI consistency and readability. Moved MainInfo to separate component and passed data with props

--- src/features/weather/WeatherDisplay/index.tsx
+++ src/features/weather/WeatherDisplay/index.tsx
@@ -1,20 +1,10 @@
-import Lottie from "react-lottie";
 import TranspContainer from "../../../components/TranspContainer";
-import {
-  formatDescription,
-  formatTemperature,
-} from "../../../utils/dataFormatting";
-import { getWeatherAnimation } from "../../../utils/getWeatherAnimation";
-import { useCurrentDate } from "../../../hooks/useCurrentDate";
 import Loader from "../../../components/Loader";
-import Clock from "../../../components/Clock";
 import { AiData, GeocodingData, WeatherResponse } from "../../../types/types";
-import BarometerIcon from "../../../assets/icons/barometer.svg?react";
-import LocationIcon from "../../../assets/icons/location.svg?react";
-import CalendarIcon from "../../../assets/icons/calendar.svg?react";
 import StarsIcon from "../../../assets/icons/stars.svg?react";
 import DetailedInfo from "./DetailedInfo";
 import Button from "../../../components/Button";
+import MainInfo from "./MainInfo";
 
 interface WeatherDisplayProps {
   selectedLocation: GeocodingData | null;
@@ -34,17 +24,8 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
   aiRequest,
   aiData,
 }) => {
-  const defaultOptions = (animationData: string) => ({
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  });
   const { isPending, isWeatherFetching, data, error } = weatherData;
   const { isPending: isAILoading } = aiData;
-  const currentDate = useCurrentDate();
 
   return (
     <>
@@ -61,43 +42,10 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
               <div className="flex w-full">
                 <article className="flex ">
                   <div className="flex gap-24 justify-between w-full">
-                    <div>
-                      <h3 className="font-semibold text-xl">Now</h3>
-                      <div className="flex items-center my-2">
-                        <LocationIcon width={40} height={40} />
-                        <h4 className="font-normal text-md text-gray-800">
-                          {`${selectedLocation?.name}, ${data?.name}`}
-                        </h4>
-                      </div>
-                      <div className="flex items-center">
-                        <CalendarIcon />
-                        <h4 className="font-normal text-md text-gray-600">
-                          {currentDate}, {""}
-                          {data ? <Clock timezone={data.timezone} /> : null}
-                        </h4>
-                      </div>
-
-                      <p className="text-lg font-light py-2">
-                        {data
-                          ? formatDescription(data.weather[0].description)
-                          : null}
-                      </p>
-                      <div className="flex items-center justify-center gap-4">
-                        {data ? (
-                          <Lottie
-                            options={defaultOptions(
-                              getWeatherAnimation(data.weather[0].main)
-                            )}
-                            height={120}
-                            width={120}
-                          />
-                        ) : null}
-
-                        <p className="text-6xl font-semibold py-2">
-                          {data ? formatTemperature(data.main.temp) : null}°C
-                        </p>
-                      </div>
-                    </div>
+                    <MainInfo
+                      weather={data}
+                      selectedLocation={selectedLocation}
+                    />
                     <div className="flex flex-col gap-4">
                       <div className="flex self-end gap-4 items-center absolute translate-x-9 -translate-y-4">
                         {isAILoading ? (
@@ -125,9 +73,3 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({
 };
 
 export default WeatherDisplay;
-
-{
-  /* <div>
-        <Lottie options={defaultOptions} height={50} width={50} />
-      </div> */
-}
