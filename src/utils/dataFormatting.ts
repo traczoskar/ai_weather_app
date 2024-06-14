d3119acf6cc88cc10e8c06fd0a58ee01c3adc058
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-14 18:30:49 +0200

    refactor: Update temperature formatting and time display in dataFormatting.ts

--- src/utils/dataFormatting.ts
+++ src/utils/dataFormatting.ts
@@ -2,7 +2,14 @@
 export const formatDescription = (description: string) =>
   description.charAt(0).toUpperCase() + description.slice(1);
 
-export const formatTemperature = (temp: number) => temp.toFixed(1);
+export const formatPrimaryTemp = (temp: number) => temp.toFixed(0);
+
+export const formatSecondaryTemp = (temp: number) => temp.toFixed(1);
 
 export const formatTime = (time: number) =>
   new Date(time * 1000).toLocaleTimeString();
+
+export const formatHoursMinutes = (time: number) =>
+  new Date(time * 1000).toLocaleTimeString().slice(0, 5);
+
+export const formatWindSpeed = (speed: number) => (speed * 3.6).toFixed(1);
