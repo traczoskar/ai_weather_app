da6cb754b44c302a508b754bf9d1763127ed67c0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-16 15:00:20 +0200

    feat: Add useAirPollutionData hook for fetching air pollution data

---
+++ src/hooks/api/useAirPollutionData.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchAirPollution = async (lat: number, lon: number) => {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${
+      import.meta.env.VITE_API_KEY
+    }`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch air pollution data!");
+  }
+
+  return await response.json();
+};
+
+export const useAirPollutionData = (lat: number | null, lon: number | null) => {
+  return useQuery({
+    queryKey: lat && lon ? ["air-pollution", lat, lon] : [],
+    queryFn: () => fetchAirPollution(lat!, lon!),
+    enabled: !!lat && !!lon,
+  });
+};
