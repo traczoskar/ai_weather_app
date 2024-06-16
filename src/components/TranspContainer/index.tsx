9b3acb9f03ef44ea698bedaee03fdea46aba3c96
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-16 14:59:59 +0200

    refactor: Update TranspContainer component styling for improved UI consistency and readability. Increase padding and adjust rounded corners.

--- src/components/TranspContainer/index.tsx
+++ src/components/TranspContainer/index.tsx
@@ -4,7 +4,7 @@ interface TranspContainerProps {
 
 const TranspContainer = ({ children }: TranspContainerProps) => {
   return (
-    <div className="flex bg-white p-10 rounded-3xl w-full shadow-lg">
+    <div className="flex bg-white p-14 rounded-3xl w-full shadow-lg">
       {children}
     </div>
   );
