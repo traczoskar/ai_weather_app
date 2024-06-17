fdcfc0da685c47208e413dd0d984f630e5dde594
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-17 21:29:57 +0200

    refactor: Improve button styling with shadow and hover effects

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<ButtonProps> = ({ onClick, icon, children }) => {
   return (
     <button
       onClick={onClick}
-      className="flex gap-2 hover:cursor-pointer text-sky-800 border transition-all border-sky-200 bg-sky-100 text-sm font-normal hover:border-sky-700 active:bg-sky-200 active:scale-90 px-3 py-2 rounded-xl items-center"
+      className="flex gap-2 hover:cursor-pointer shadow-sm text-sky-800 border transition-all border-sky-200 bg-sky-100 text-sm font-normal hover:border-sky-700 active:bg-sky-200 active:scale-90 px-3 py-2 rounded-xl items-center"
     >
       {icon ? <span className="text-fuchsia-700">{icon}</span> : null}
       {children}
