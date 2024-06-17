3a13494c6fba9884aac0a65c557d64610ede3b87
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-17 21:30:22 +0200

    refactor: Update SuggestionDisplay component to handle selected location changes

--- src/features/suggestions/SuggestionDisplay/index.tsx
+++ src/features/suggestions/SuggestionDisplay/index.tsx
@@ -1,14 +1,28 @@
 import TranspContainer from "../../../components/TranspContainer";
 import ReactMarkdown from "react-markdown";
 import Loader from "../../../components/Loader";
-import { AiData } from "../../../types/types";
+import { GeocodingData, QueryData } from "../../../types/types";
+import { useEffect, useState } from "react";
 
 interface SuggestionsDisplayProps {
-  aiData: AiData;
+  aiData: QueryData;
+  selectedLocation: GeocodingData | null;
 }
 
-const SuggestionDisplay: React.FC<SuggestionsDisplayProps> = ({ aiData }) => {
+const SuggestionDisplay: React.FC<SuggestionsDisplayProps> = ({
+  aiData,
+  selectedLocation,
+}) => {
   const { isPending, data, error } = aiData;
+  const [aiResponse, setAiResponse] = useState<string | null>(null);
+
+  useEffect(() => {
+    setAiResponse(data);
+  }, [data]);
+
+  useEffect(() => {
+    setAiResponse(null);
+  }, [selectedLocation]);
 
   {
     isPending && <Loader borderColor="black" />;
@@ -19,7 +33,7 @@ const SuggestionDisplay: React.FC<SuggestionsDisplayProps> = ({ aiData }) => {
 
   return (
     <>
-      {data && (
+      {aiResponse && (
         <TranspContainer>
           <section className="flex flex-col w-auto">
             <h2 className="text-2xl font-bold mb-4">AI suggestion:</h2>
