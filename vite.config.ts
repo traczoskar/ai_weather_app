dd89400b180d1ef17b78ac9ebb5e392c0564e116
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-18 12:46:15 +0200

    refactor: Update vite.config.ts to include base URL and environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      },
-      include: "**/*.svg?react",
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          icon: true,
+        },
+        include: "**/*.svg?react",
+      }),
+    ],
+    base: "/ai_weather_app/",
+    define: {
+      "process.env": env,
+    },
+    build: {
+      outDir: "build",
+    },
+  };
 });
