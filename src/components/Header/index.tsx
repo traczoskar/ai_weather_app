dd9099e168d9d17e020bb5d5b247ea1afc2ce8e8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-18 23:11:56 +0200

    refactor: Update Header component with responsive layout and dark mode support

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,8 +8,8 @@ interface HeaderProps {
 
 const Header = ({ title, setSelectedLocation }: HeaderProps) => {
   return (
-    <header className="flex justify-between items-center w-full mt-6">
-      <div className="flex text-sky-800 drop-shadow-lg items-center">
+    <header className="flex flex-col md:flex-row gap-6  justify-between items-center w-full mt-6">
+      <div className="flex text-sky-800 dark:text-sky-300 dark:drop-shadow-md drop-shadow-lg items-center">
         <AppLogo className="w-8 h-8 mr-3" />
         <h1 className="text-3xl font-bold font-['Raleway'] ">{title}</h1>
       </div>
