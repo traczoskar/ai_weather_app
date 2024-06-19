8ee17efd901b412af0447cdad26baa41363bb05f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 08:34:29 +0200

    refactor: Update Select component with dark mode support

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -7,8 +7,8 @@ const Select = ({ onClick, children }: SelectProps) => {
   return (
     <div
       onClick={onClick}
-      className="w-full px-3 py-2 text-sm  bg-white
-        hover:bg-sky-100 hover:border-slate-300 hover:border hover:cursor-pointer"
+      className="w-full px-3 py-2 text-sm  bg-white dark:bg-slate-700 dark:hover:bg-slate-800
+        hover:bg-sky-100 hover:border-slate-300 hover:border  hover:cursor-pointer"
     >
       {children}
     </div>
