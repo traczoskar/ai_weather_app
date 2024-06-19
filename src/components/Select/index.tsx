36c2160dc7b352f754111ff1c9921195550fe914
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 23:33:32 +0200

    refactor: Update Select component background colors

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -7,7 +7,7 @@ const Select = ({ onClick, children }: SelectProps) => {
   return (
     <div
       onClick={onClick}
-      className="w-full px-4 py-4 text-sm  bg-white dark:bg-slate-700 dark:hover:bg-slate-800
+      className="w-full px-4 py-4 text-sm  bg-white dark:bg-sky-950 dark:hover:bg-sky-800
         hover:bg-sky-100 hover:border-slate-300 hover:border  hover:cursor-pointer"
     >
       {children}
