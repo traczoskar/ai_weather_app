cd3a157156c7d5ddf6837d3726e8f5332c1f6444
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 17:10:14 +0200

    refactor: Update Select component styling for better readability and accessibility

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -7,7 +7,7 @@ const Select = ({ onClick, children }: SelectProps) => {
   return (
     <div
       onClick={onClick}
-      className="w-full px-3 py-2 text-sm  bg-white dark:bg-slate-700 dark:hover:bg-slate-800
+      className="w-full px-4 py-4 text-sm  bg-white dark:bg-slate-700 dark:hover:bg-slate-800
         hover:bg-sky-100 hover:border-slate-300 hover:border  hover:cursor-pointer"
     >
       {children}
