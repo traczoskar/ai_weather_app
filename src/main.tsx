0647293f144cd0a50ebdd4cf9eac2b92a9d019d4
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 08:34:10 +0200

    refactor: Add Provider component for Redux store integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import App from "./core/App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Provider } from "react-redux";
+import store from "./core/store.ts";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools />
-      <App />
+      <Provider store={store}>
+        <ReactQueryDevtools />
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
