f66cec47ce17db4e54c33b8a6d3d42d07dd2f9d6
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 08:34:23 +0200

    refactor: Update Header component with transition-colors for smoother color change

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Header = ({ title, setSelectedLocation }: HeaderProps) => {
   return (
     <header className="flex flex-col md:flex-row gap-6  justify-between items-center w-full mt-6">
-      <div className="flex text-sky-800 dark:text-sky-300 dark:drop-shadow-md drop-shadow-lg items-center">
+      <div className="flex text-sky-800 dark:text-sky-300 dark:drop-shadow-md drop-shadow-lg items-center transition-colors">
         <AppLogo className="w-8 h-8 mr-3" />
         <h1 className="text-3xl font-bold font-['Raleway'] ">{title}</h1>
       </div>
