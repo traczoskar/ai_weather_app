2c4393bd2a7fde8d9e071735f5a69f1b7c66800a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 22:33:40 +0200

    refactor: Update TranspContainer component styles to include border and transition effects

--- src/components/TranspContainer/index.tsx
+++ src/components/TranspContainer/index.tsx
@@ -4,7 +4,7 @@ interface TranspContainerProps {
 
 const TranspContainer = ({ children }: TranspContainerProps) => {
   return (
-    <div className="flex bg-white dark:bg-sky-700 transition-colors sm:p-14 p-7 rounded-3xl w-full shadow-lg">
+    <div className="flex bg-white dark:bg-sky-800 border-t border-t-sky-50 dark:border-t-sky-700 transition-colors sm:p-14 p-7 rounded-3xl w-full shadow-lg">
       {children}
     </div>
   );
