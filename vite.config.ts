0f9dfedd811b3708d9899c63937d527fe56789ed
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 22:33:28 +0200

    refactor: Update deployment workflow and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [
-      react(),
-      svgr({
-        svgrOptions: {
-          icon: true,
-        },
-        include: "**/*.svg?react",
-      }),
-    ],
-    base: "/ai_weather_app/",
-    define: {
-      "process.env": env,
-    },
-    build: {
-      outDir: "build",
-    },
-  };
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+      include: "**/*.svg?react",
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
 });
