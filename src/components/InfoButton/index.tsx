e5d8c1bba2e06f3770baa51f427d7fc15340769c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-19 21:02:16 +0200

    feat: Add InfoButton component

---
+++ src/components/InfoButton/index.tsx
@@ -0,0 +1,16 @@
+interface InfoButtonProps {
+  onClick: () => void;
+}
+
+const InfoButton: React.FC<InfoButtonProps> = ({ onClick: openInfo }) => {
+  return (
+    <button
+      onClick={openInfo}
+      className="p-4 flex items-center w-6 h-6 justify-center drop-shadow-lg rounded-full font-bold text-sky-700 dark:text-sky-300 transition-all text-2xl hover:scale-125 hover:brightness-110 hover:bg-sky-100 dark:hover:bg-sky-700"
+    >
+      ?
+    </button>
+  );
+};
+
+export default InfoButton;
