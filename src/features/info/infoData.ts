569745863aebb25d989bb4da7b7bbd8d804644f5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-20 14:06:33 +0200

    refactor: created infoData.ts file for storing necessary data for InfoDisplay component

---
+++ src/features/info/infoData.ts
@@ -0,0 +1,45 @@
+export const INFO_DATA = {
+  title: "Hello! 🙋🏻‍♂️",
+  subTitle: "Thanks for discovering my App!",
+  description:
+    "Dive in and explore all its features. Feel free to try it out in every way you want, and let me know if there's anything we can improve. Here's what makes our app awesome:",
+  keyFeatures: [
+    {
+      feature: "🌡️ Current Weather",
+      text: "Get real-time weather updates for your location.",
+    },
+    {
+      feature: "🏙️ Location Search",
+      text: "Easily find weather info by searching for any city.",
+    },
+    {
+      feature: "📍 Geolocation",
+      text: "Allow your browser to detect your location for instant weather data.",
+    },
+    {
+      feature: "📅 5-Day Forecast",
+      text: "Plan ahead with accurate 5-day weather forecasts.",
+    },
+    {
+      feature: "🌬️ Air Quality",
+      text: "Check the current air quality for your area.",
+    },
+    {
+      feature: "🌗 Themes",
+      text: "Switch between light and dark themes, or let the app match your browser's preference.",
+    },
+    {
+      feature: "📱 Responsive Design",
+      text: "Enjoy a seamless experience on any device.",
+    },
+  ],
+  builtWith: [
+    "React",
+    "TypeScript",
+    "TailwindCSS",
+    "Framer Motion",
+    "OpenWeatherMap API",
+    "Redux",
+    " Vite",
+  ],
+};
