87a3b68acddb81ccce9be2c1e632cda5afc57be0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-20 10:55:01 +0200

    refactor: Add framer-motion animation to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import WeatherForm from "../../features/weather/WeatherForm";
 import AppLogo from "../../assets/icons/app_logo.svg?react";
 import ThemeSwitch from "../ThemeSwitch";
+import { motion } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsDarkMode, toggleDarkMode } from "../../slices/themeSlice";
 import { useEffect } from "react";
@@ -38,14 +39,24 @@ const Header = ({ title, setSelectedLocation, openInfo }: HeaderProps) => {
   return (
     <header className="w-full flex flex-col gap-6">
       <div className="flex flex-col min-[415px]:flex-row gap-6 justify-between items-center w-full mt-6">
-        <div className="flex text-sky-800 dark:text-sky-300 dark:drop-shadow-md drop-shadow-lg items-center transition-colors">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          className="flex text-sky-800 dark:text-sky-300 dark:drop-shadow-md drop-shadow-lg items-center transition-colors"
+        >
           <AppLogo className="w-8 h-8 mr-3" />
           <h1 className="text-3xl font-bold font-['Raleway'] ">{title}</h1>
-        </div>
-        <div className="flex justify-center items-center gap-4">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          className="flex justify-center items-center gap-4"
+        >
           <InfoButton onClick={openInfo} />
           <ThemeSwitch />
-        </div>
+        </motion.div>
       </div>
       <WeatherForm setSelectedLocation={setSelectedLocation} />
     </header>
