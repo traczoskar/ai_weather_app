faa0ce4967b50011103eb2185bf3dee2a9b99c0a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-20 10:55:27 +0200

    refactor: Add framer-motion animation to MainInfo component

--- src/features/weather/WeatherDisplay/MainInfo/index.tsx
+++ src/features/weather/WeatherDisplay/MainInfo/index.tsx
@@ -8,6 +8,7 @@ import {
   formatPrimaryTemp,
 } from "../../../../utils/dataFormatting";
 import Lottie from "lottie-react";
+import { motion } from "framer-motion";
 import { getWeatherAnimation } from "../../../../utils/getWeatherAnimation";
 import { useMediaQuery } from "react-responsive";
 
@@ -34,32 +35,55 @@ const MainInfo: React.FC<MainInfoProps> = ({ weather, selectedLocation }) => {
   return (
     <div className="flex lg:flex-col justify-between gap-4 pb-8 lg:pb-0">
       <div className="flex flex-col gap-2 sm:gap-4">
-        <h2 className="font-bold text-[6vw] sm:text-2xl max-w-xs drop-shadow text-sky-700 dark:text-sky-50">
+        <motion.h2
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+          className="font-bold text-[6vw] sm:text-2xl max-w-xs drop-shadow text-sky-700 dark:text-sky-50"
+        >
           {selectedLocation?.name}
-        </h2>
-        <div className="flex items-center gap-3 text-slate-500 dark:text-sky-300">
+        </motion.h2>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+          className="flex items-center gap-3 text-slate-500 dark:text-sky-300"
+        >
           <LocationIcon className="w-4 h-4 sm:w-6 sm:h-6 md:w-7 md:h-7" />
           <h4 className="font-normal text-[3.5vw] sm:text-lg">
             {weather?.name}
             {selectedLocation?.country ? `, ${selectedLocation.country}` : null}
           </h4>
-        </div>
-        <div className="flex items-center gap-3  text-slate-500 dark:text-sky-300">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
+          className="flex items-center gap-3  text-slate-500 dark:text-sky-300"
+        >
           <CalendarIcon className="sm:w-6 sm:h-6 md:w-7 md:h-7" />
           <h4 className="font-normal text-[3.5vw] sm:text-lg tracking-wide">
             {currentDate}, {""}
             {weather ? <Clock timezone={weather.timezone} /> : null}
           </h4>
-        </div>
-        <p
+        </motion.div>
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.2 }}
           className="text-md sm:text-xl font-semibold
        text-sky-600 dark:text-sky-100 pt-0 sm:pt-2"
         >
           {weather ? formatDescription(weather.weather[0].description) : null}
-        </p>
+        </motion.p>
       </div>
 
-      <div className="flex flex-col sm:flex-row items-center justify-center self-end gap-2 sm:gap-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.2 }}
+        className="flex flex-col sm:flex-row items-center justify-center self-end gap-2 sm:gap-4"
+      >
         {weather && animationData ? (
           <Lottie
             animationData={animationData}
@@ -75,7 +99,7 @@ const MainInfo: React.FC<MainInfoProps> = ({ weather, selectedLocation }) => {
         <p className="text-[8.5vw] sm:text-5xl md:text-6xl font-semibold text-sky-900 dark:text-sky-200 drop-shadow-md">
           {weather ? formatPrimaryTemp(weather.main.temp) : null}°C
         </p>
-      </div>
+      </motion.div>
     </div>
   );
 };
