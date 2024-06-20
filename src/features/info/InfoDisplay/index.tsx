0ffeb4e4d19aa9ed3d4f4964ad3dbd44ed6a5ec2
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-20 14:16:33 +0200

    refactor: Update InfoDisplay component with CloseIcon and Footer

--- src/features/info/InfoDisplay/index.tsx
+++ src/features/info/InfoDisplay/index.tsx
@@ -13,18 +13,18 @@ const InfoDisplay: React.FC<InfoDisplayProps> = ({ isInfoOpen, closeInfo }) => {
     <>
       {isInfoOpen && (
         <div className="flex justify-center items-center w-full h-full fixed left-0 top-0 backdrop-blur-md z-40 ">
+          <button
+            onClick={closeInfo}
+            className="fixed flex justify-center items-center p-1 rounded-full text-sky-600 dark:text-sky-800 bg-sky-200 top-4 left-4 hover:brightness-105 hover:scale-105 active:scale-90 transition-all"
+          >
+            <CloseIcon />
+          </button>
           <motion.div
             className="flex flex-col w-[90vw] h-[90vh] z-50 relative lg:max-w-[90vw] lg:max-h-[90vh] p-8 sm:p-16 rounded-2xl gap-4  shadow-xl items-center  border-t dark:border-t-sky-700 bg-sky-900 border-t-sky-300 dark:bg-sky-800 bg-opacity-80 overflow-auto dark:bg-opacity-70"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <button
-              onClick={closeInfo}
-              className="absolute flex justify-center items-center p-1 rounded-full text-sky-600 dark:text-sky-800 bg-sky-200 top-6 right-6 hover:brightness-105 hover:scale-105 active:scale-90 transition-all"
-            >
-              <CloseIcon />
-            </button>
             <article className="flex flex-col items-center justify-center relative gap-8">
               <h2 className="text-sky-50 font-bold flex flex-col sm:flex-row items-center gap-2 text-[3.8vw] sm:text-xl md:text-2xl lg:text-3xl drop-shadow-sm">
                 {INFO_DATA.title}
@@ -77,7 +77,7 @@ const InfoDisplay: React.FC<InfoDisplayProps> = ({ isInfoOpen, closeInfo }) => {
                 with a backend is needed to act as a bridge between the frontend
                 application and OpenAI (mainly for security purposes).
               </p>
-              <p className="text-sky-200  text-lg drop-shadow-sm">
+              <p className="text-sky-200  text-md tracking-wide md:text-lg drop-shadow-sm">
                 I am currently working on this solution and will soon release a
                 fully functional version.{" "}
                 <strong>
@@ -86,7 +86,7 @@ const InfoDisplay: React.FC<InfoDisplayProps> = ({ isInfoOpen, closeInfo }) => {
                 </strong>{" "}
                 How to do it?:
               </p>
-              <ol className="list-decimal  text-sky-200  text-lg drop-shadow-sm self-start pl-8 marker:font-bold bg-sky-900 py-1 my-4 marker:text-sky-400">
+              <ol className="list-decimal  text-sky-200  text-md md:text-lg drop-shadow-sm self-start pl-8 marker:font-bold bg-sky-900 py-1 my-4 marker:text-sky-400">
                 <li className="list-item tracking-wide px-2 py-2 ">
                   Download this repository
                 </li>
@@ -101,7 +101,7 @@ const InfoDisplay: React.FC<InfoDisplayProps> = ({ isInfoOpen, closeInfo }) => {
                   Start the development server.
                 </li>
               </ol>{" "}
-              <span className="font-bold text-xl self-start text-sky-200">
+              <span className="font-bold  text-lg md:text-xl self-start text-sky-200">
                 Let me know how you like this feature! 🔥🚀
               </span>
               <Footer isOnInfoDisplay={true} />
