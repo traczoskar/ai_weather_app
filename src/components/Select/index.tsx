a687b4b75b524ae2f95f41f8e35f56fe557a2d0b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-21 10:21:21 +0200

    refactor: Add accessibility attributes to Select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -13,6 +13,8 @@ const Select = ({ onClick, children }: SelectProps) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
       onClick={onClick}
+      aria-label="Select the location"
+      role="button"
       className="w-full px-4 py-4 text-sm  bg-white overflow-hidden dark:bg-sky-950 dark:hover:bg-sky-800
         hover:bg-sky-100 hover:border-slate-300 hover:border  hover:cursor-pointer"
     >
