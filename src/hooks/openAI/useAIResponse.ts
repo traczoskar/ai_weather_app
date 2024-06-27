6dc2770faaf9d313efc31c3e260d0b9a4227a219
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-27 15:49:57 +0200

    refactor: Update useAIResponse hook to use fetch API for AI query

--- src/hooks/openAI/useAIResponse.ts
+++ src/hooks/openAI/useAIResponse.ts
@@ -1,26 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPEN_AI_KEY,
-  dangerouslyAllowBrowser: true,
-});
-
-const requestFunction = async (content: any) => {
+const queryFunction = async (content: any) => {
   try {
-    const prompt = await openai.chat.completions.create({
-      messages: content,
-      model: "gpt-3.5-turbo",
+    const response = await fetch(import.meta.env.VITE_MAKE_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(content),
     });
-    const response = prompt.choices[0].message.content;
-    return response;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("response", data);
+    return data;
   } catch (error) {
     console.error("Error fetching AI completion:", error);
     return error;
   }
 };
+
 const getAiResponse = async (systemMessage: string, userMessage: string) => {
-  const answer = await requestFunction([
+  const answer = await queryFunction([
     {
       role: "system",
       content: systemMessage,
@@ -37,6 +39,7 @@ export const useAIResponse = (systemMessage: string, userMessage: string) => {
   return useQuery({
     queryKey: ["ai", systemMessage, userMessage],
     queryFn: () => getAiResponse(systemMessage, userMessage),
+    retry: false,
     enabled: false,
     staleTime: Infinity,
     gcTime: Infinity,
