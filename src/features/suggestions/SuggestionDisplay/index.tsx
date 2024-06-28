f5325dc59b3a664777f89d923925f3c7551a7b63
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-28 21:33:48 +0200

    refactor: Update SuggestionDisplay component to enhance AI suggestion rendering

--- src/features/suggestions/SuggestionDisplay/index.tsx
+++ src/features/suggestions/SuggestionDisplay/index.tsx
@@ -1,10 +1,11 @@
 import TranspContainer from "../../../components/TranspContainer";
-import ReactMarkdown from "react-markdown";
 import Loader from "../../../components/Loader";
 import { GeocodingData, QueryData } from "../../../types/types";
 import { useEffect, useState } from "react";
+import LocationIcon from "../../../assets/icons/location.svg?react";
 import StarsIcon from "../../../assets/icons/stars.svg?react";
 import Button from "../../../components/Button";
+import SuggestionTile from "../SuggestionTile";
 
 interface SuggestionsDisplayProps {
   aiData: QueryData;
@@ -56,12 +57,71 @@ const SuggestionDisplay: React.FC<SuggestionsDisplayProps> = ({
       )}
       {aiResponse && (
         <TranspContainer>
-          <section className="flex flex-col w-auto">
-            <h2 className="text-2xl font-bold mb-4 dark:text-sky-100">
-              AI suggestion:
-            </h2>
-            <article className="text-md text-gray-700 dark:text-white">
-              <ReactMarkdown>{data[0].response}</ReactMarkdown>
+          <section className="flex flex-col w-full">
+            <div className="flex justify-between w-full">
+              <h2 className="text-sky-700 transition-colors dark:text-sky-200 text-2xl font-semibold flex gap-4 drop-shadow items-center">
+                AI advices for today {`at ${data.location}`}
+                <StarsIcon width={28} height={28} />
+              </h2>
+              <p className="flex gap-2 text-sky-600 transition-colors dark:text-sky-300 text-md drop-shadow items-center ">
+                <LocationIcon width={20} height={20} /> {data.location},{" "}
+                {data.date}
+              </p>
+            </div>
+
+            <article className="flex flex-col text-md text-gray-700 dark:text-white mt-8">
+              <SuggestionTile
+                general_advice={{
+                  title: "One phrase from AI:",
+                  text: data.suggestions.general_advice,
+                }}
+                mood={{
+                  title: "Mood:",
+                  text: data.suggestions.mood,
+                }}
+              />
+              <div className="grid grid-cols-2 gap-4 text-md mt-4">
+                <SuggestionTile
+                  title="Indoor Activities"
+                  data={data.suggestions.indoor_activities}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Outdoor Activities"
+                  data={data.suggestions.outdoor_activities}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Attire"
+                  data={data.suggestions.attire}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Food Suggestions"
+                  data={data.suggestions.food_suggestions}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Health Tips"
+                  data={data.suggestions.health_tips}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Places to Visit"
+                  data={data.suggestions.places_to_visit}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Music"
+                  data={data.suggestions.music}
+                  isArray={true}
+                />
+                <SuggestionTile
+                  title="Movies"
+                  data={data.suggestions.movies}
+                  isArray={true}
+                />
+              </div>
             </article>
           </section>
         </TranspContainer>
