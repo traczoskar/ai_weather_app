d6fc273d676968a62e0b2fa21646179efee3206f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-29 03:15:38 +0200

    refactor: Update Button component styles for dark mode support

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<ButtonProps> = ({ onClick, icon, children }) => {
   return (
     <button
       onClick={onClick}
-      className="flex gap-2 hover:cursor-pointer shadow-sm text-sky-800 border transition-all border-sky-200 bg-sky-100 text-sm font-normal hover:border-sky-700 active:bg-sky-200 active:scale-90 px-3 py-2 rounded-xl items-center"
+      className="flex gap-2 hover:cursor-pointer shadow-sm hover:shadow-md text-sky-800 dark:text-sky-100 border transition-all border-sky-200 dark:border-sky-700 bg-sky-100 dark:bg-sky-900 text-sm font-normal hover:border-sky-700 dark:hover:border-sky-500 active:bg-sky-200 active:scale-90 px-3 py-2 rounded-xl items-center"
     >
       {icon ? <span className="text-fuchsia-700">{icon}</span> : null}
       {children}
