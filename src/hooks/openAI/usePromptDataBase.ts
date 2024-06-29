be6e585526f4c5705ea99fc8682291feef95a312
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-29 13:50:35 +0200

    refactor: Update usePromptDataBase hook to handle null weatherData.name and include selectedLocation parameter

--- src/hooks/openAI/usePromptDataBase.ts
+++ src/hooks/openAI/usePromptDataBase.ts
@@ -80,7 +80,9 @@ export const usePromptDataBase = (
   const userMessage = `Weather data:
 - current date: ${currentDate} r.,
 - location: ${
-    selectedLocation?.name ? selectedLocation.name : weatherData?.name
+    selectedLocation?.name && selectedLocation.name !== "Browser Location"
+      ? selectedLocation.name
+      : weatherData?.name
   },
 ${
   selectedLocation?.country
