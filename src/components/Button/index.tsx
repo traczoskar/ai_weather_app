002ed44c4d86623803c0db0cf3b1acd800ed3ae9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-29 17:10:50 +0200

    refactor: Update Button component to include disabled state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,28 @@ interface ButtonProps {
   onClick: () => void;
   icon?: React.ReactNode;
   children: React.ReactNode;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, icon, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  icon,
+  children,
+  disabled,
+}) => {
   return (
     <button
+      aria-label="Get AI Advice"
+      title="Get AI Advice"
       onClick={onClick}
-      className="flex gap-2 hover:cursor-pointer shadow-sm hover:shadow-md text-sky-800 dark:text-sky-100 border transition-all border-sky-200 dark:border-sky-700 bg-sky-100 dark:bg-sky-900 text-sm font-normal hover:border-sky-700 dark:hover:border-sky-500 active:bg-sky-200 active:scale-90 px-3 py-2 rounded-xl items-center"
+      disabled={disabled}
+      className="flex gap-2  text-sky-100 text-sm bg-sky-800 dark:bg-sky-700 disabled:text-slate-600 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:hover:scale-100 border-t-sky-900 shadow-md border-b-0 border-x-0 dark:border-t-sky-600 dark:hover:border-sky-500 rounded-full border active:scale-90 active:brightness-110 transition-all hover:scale-105 px-4 py-1.5  items-center"
     >
-      {icon ? <span className="text-fuchsia-700">{icon}</span> : null}
+      {icon ? (
+        <span className={disabled ? "text-slate-600" : "text-fuchsia-400"}>
+          {icon}
+        </span>
+      ) : null}
       {children}
     </button>
   );
