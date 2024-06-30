6bda079d24ba46c2b28bdf6ded04ad78bf2d9233
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-30 12:50:49 +0200

    refactor: Update TranspContainer component to include collapsible behavior and responsive padding

--- src/components/TranspContainer/index.tsx
+++ src/components/TranspContainer/index.tsx
@@ -1,10 +1,15 @@
 interface TranspContainerProps {
   children: React.ReactNode;
+  isCollapsed?: boolean;
 }
 
-const TranspContainer = ({ children }: TranspContainerProps) => {
+const TranspContainer = ({ children, isCollapsed }: TranspContainerProps) => {
   return (
-    <div className="flex bg-white dark:bg-sky-800 border-t border-t-sky-50 dark:border-t-sky-700 transition-colors sm:p-14 p-7 rounded-3xl w-full shadow-lg">
+    <div
+      className={`${
+        isCollapsed ? "sm:px-14 sm:py-6 px-7 py-2" : "sm:p-14 p-7"
+      } flex bg-white  dark:bg-sky-800 border-t border-t-sky-50 dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-3xl w-full shadow-lg`}
+    >
       {children}
     </div>
   );
