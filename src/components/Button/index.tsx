e4962c1957561ec2f842da096234e76282dc7e3c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-06-30 12:50:37 +0200

    refactor: Update Button component to include responsive text size and fix disabled state

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -17,7 +17,7 @@ const Button: React.FC<ButtonProps> = ({
       title="Get AI Advice"
       onClick={onClick}
       disabled={disabled}
-      className="flex gap-2  text-sky-100 text-sm bg-sky-800 dark:bg-sky-700 disabled:text-slate-600 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:hover:scale-100 disabled:border-none border-t-sky-900 shadow-md border-b-0 border-x-0 dark:border-t-sky-600 dark:hover:border-sky-500 rounded-full border active:scale-90 active:brightness-110 transition-all hover:scale-105 px-4 py-1.5  items-center"
+      className="flex gap-2  text-sky-100 text-xs min-[335px]:text-sm bg-sky-800 dark:bg-sky-700 disabled:text-slate-600 disabled:bg-slate-300 disabled:cursor-not-allowed disabled:hover:scale-100 disabled:border-none border-t-sky-900 shadow-md border-b-0 border-x-0 dark:border-t-sky-600 dark:hover:border-sky-500 rounded-full border active:scale-90 active:brightness-110 transition-all hover:scale-105 px-4 py-1.5  items-center"
     >
       {icon ? (
         <span className={disabled ? "text-slate-600" : "text-fuchsia-400"}>
