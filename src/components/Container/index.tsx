9e813c44d556f1f0a226a2ca4e8200a481b61a06
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-01 08:49:37 +0200

    refactor: Update Container component to include collapsible behavior and responsive padding

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,17 +1,31 @@
 interface ContainerProps {
   children: React.ReactNode;
   isCollapsed?: boolean;
+  isButton?: boolean;
 }
 
-const Container = ({ children, isCollapsed }: ContainerProps) => {
+const Container = ({ children, isCollapsed, isButton }: ContainerProps) => {
   return (
-    <div
-      className={`${
-        isCollapsed ? "sm:px-14 sm:py-6 px-7 py-2" : "sm:p-14 p-7"
-      } flex bg-white  dark:bg-sky-800 border-t border-t-sky-50 dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-3xl w-full shadow-lg`}
-    >
-      {children}
-    </div>
+    <>
+      {!isButton && (
+        <div
+          className={`${
+            isCollapsed
+              ? "sm:px-14 sm:pt-12 sm:pb-14 px-7 pb-y pt-5"
+              : "sm:p-14 p-7"
+          } flex bg-white  dark:bg-sky-800 border-t  border-t-sky-50 dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-3xl w-full shadow-lg`}
+        >
+          {children}
+        </div>
+      )}
+      {isButton && (
+        <div
+          className={`sm:px-6 sm:py-3 px-7 py-2 flex bg-white hover:scale-[1.02] hover:brightness-105 dark:bg-sky-800 border-t border-t-sky-50 dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-2xl w-full shadow-lg`}
+        >
+          {children}
+        </div>
+      )}
+    </>
   );
 };
 
