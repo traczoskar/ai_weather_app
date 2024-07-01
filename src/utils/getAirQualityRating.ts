b55510c87cb28598b7495a25d9c4f65d9efa8b06
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-01 19:48:46 +0200

    refactor: Update air quality rating labels in getAirQualityRating

--- src/utils/getAirQualityRating.ts
+++ src/utils/getAirQualityRating.ts
@@ -2,7 +2,7 @@ export const getAirQualityRating = (value: number) => {
   if (value === 1) return "Very Good";
   if (value === 2) return "Good";
   if (value === 3) return "Moderate";
-  if (value === 4) return "Poor";
-  if (value === 5) return "Bad";
+  if (value === 4) return "Bad";
+  if (value === 5) return "Very Bad";
   return "Unknown";
 };
