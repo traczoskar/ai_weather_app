0dc17fc318bff046c688b6e7ca61e316d45adf49
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-01 08:49:49 +0200

    refactor: Update Popover component to include z-index for proper layering

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -49,7 +49,7 @@ const Popover: React.FC<PopoverProps> = ({
       transition={{ duration: 0.5 }}
       ref={popoverRef}
       aria-label="Popover"
-      className="flex items-center justify-center absolute top-16 lg:top-20 text-[10px] sm:text-xs right-2 lg:-right-6 bg-sky-100 border-sky-300 text-sky-800 dark:bg-sky-500 border dark:border-sky-400 dark:text-sky-100 shadow-lg rounded-full px-4 py-2"
+      className="flex items-center justify-center z-50 absolute top-16 lg:top-24 text-[10px] sm:text-xs right-2 lg:-right-6 bg-sky-100 border-sky-300 text-sky-800 dark:bg-sky-300 border dark:border-sky-400 dark:text-sky-700 shadow-lg rounded-full px-4 py-2"
     >
       {children}
     </motion.div>
