7069d8d0ea3892c195be769026693cc58fa40725
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-25 20:01:01 +0200

    refactor: Add data-test attribute to Select component

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,25 +1,27 @@
 import { motion } from "framer-motion";
 
 interface SelectProps {
+  index?: number;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
-const Select = ({ onClick, children }: SelectProps) => {
+const Select = ({ index, onClick, children }: SelectProps) => {
   return (
-    <motion.div
+    <motion.li
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
       onClick={onClick}
+      data-test={`location-select-${index}`}
       aria-label="Select the location"
       role="button"
-      className="w-full px-4 py-4 text-sm  bg-white overflow-hidden dark:bg-sky-950 dark:hover:bg-sky-800
+      className="w-full px-4 py-4 text-sm list-none bg-white overflow-hidden dark:bg-sky-950 dark:hover:bg-sky-800
         hover:bg-sky-100 hover:border-slate-300 hover:border  hover:cursor-pointer"
     >
       {children}
-    </motion.div>
+    </motion.li>
   );
 };
 
