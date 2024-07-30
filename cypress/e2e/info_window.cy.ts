94a70e248540c3012d76417d0c5560b6d8732252
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-30 17:06:01 +0200

    refactor: Add tests of Key Features proper rendering

--- cypress/e2e/info_window.cy.ts
+++ cypress/e2e/info_window.cy.ts
@@ -11,10 +11,28 @@ describe("Info window tests", () => {
       .should("exist")
       .and("be.visible")
       .within(() => {
+        //Info Header test
         cy.get("h2")
           .should("exist")
           .and("contain.text", INFO_DATA.title)
           .and("contain.text", INFO_DATA.subTitle);
+        cy.getDataTest("info-description")
+          .should("exist")
+          .and("contain.text", INFO_DATA.description);
+        //Key Features test
+        cy.getDataTest("info-features-header")
+          .should("exist")
+          .and("contain.text", "Key Features:");
+        cy.getDataTest("info-features")
+          .scrollIntoView()
+          .should("exist")
+          .within(() => {
+            cy.get("li").should("have.length", INFO_DATA.keyFeatures.length);
+            cy.get("li").each((li, index) => {
+              expect(li).to.contain.text(INFO_DATA.keyFeatures[index].feature);
+              expect(li).to.contain.text(INFO_DATA.keyFeatures[index].text);
+            });
+          });
       });
   });
 });
