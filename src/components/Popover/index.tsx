01bcec31805c39965a1dc389c31ee0d215fff381
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-01 19:22:31 +0200

    refactor: Add data-test attribute to Popover component

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -48,6 +48,7 @@ const Popover: React.FC<PopoverProps> = ({
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.5 }}
       ref={popoverRef}
+      data-test="ai-popover"
       aria-label="Popover"
       className="flex items-center justify-center z-50 absolute top-16 lg:top-24 text-[10px] sm:text-xs right-2 lg:-right-6 bg-sky-100 border-sky-300 text-sky-800 dark:bg-sky-300 border dark:border-sky-400 dark:text-sky-700 shadow-lg rounded-full px-4 py-2"
     >
