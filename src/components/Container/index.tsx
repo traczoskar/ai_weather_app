aafee13ab8fa091e8765c7c9b820dc5b004306b1
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-13 09:13:40 +0200

    refactor: Update Container component button styles

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -20,7 +20,7 @@ const Container = ({ children, isCollapsed, isButton }: ContainerProps) => {
       )}
       {isButton && (
         <div
-          className={`sm:px-6 sm:py-3 px-7 py-2 flex bg-sky-200 hover:scale-[1.02] dark:hover:brightness-105 border dark:bg-sky-800 dark:border-x-0 dark:border-b-0 border-sky-300 dark:border-t dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-2xl w-full shadow-md  dark:shadow-lg`}
+          className={`sm:px-6 sm:py-3 px-7 py-2 flex bg-white hover:scale-[1.02] hover:bg-sky-50 dark:hover:brightness-105 border dark:bg-sky-800 dark:border-x-0 dark:border-b-0 border-sky-50 dark:border-t dark:border-t-sky-700 transition-all sm:p-14 p-7 rounded-2xl w-full shadow-md  dark:shadow-lg`}
         >
           {children}
         </div>
